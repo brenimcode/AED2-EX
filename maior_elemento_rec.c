206c567ba72887d3e174121ddd14daba227af55f
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2024-01-12 17:34:01 -0300

    algoritmos recursivos basicos

---
+++ maior_elemento_rec.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int vet[], int tamanho,int pos_maior){
+    //CASO BASE
+    if(tamanho == 0){
+        return vet[pos_maior];
+        // Durante a recursão vamos alterando esse valor, caso desejavel
+    }// Vamos sempre reduzindo o vetor, fazendo as comparações do vet[maior],vet[tamanho-1] até que retorne o 
+    else{
+        if(vet[tamanho-1] > vet[pos_maior]){
+            return max(vet,tamanho-1,tamanho-1);
+        }
+        else{
+            return max(vet,tamanho-1,pos_maior);
+        }
+    }
+}
+
+int main(){
+    int A[5] = {5,4,7,2,1};
+
+    printf("MAIOR = [%d]",max(A,5,0));
+    return 0;
+
+}
---
+++ mdc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mdc(int a, int b){
+    //MDC de A e B, se A%B == 0 para o mdc entre eles é 
+    if(a%b == 0){
+        return b;
+    }
+    else{
+        return mdc(b,a%b);
+    }
+}
+
+int main(){
+    int a,b;
+
+    scanf("%d%d",&a,&b);
+    printf("mdc(a,b) = [%d]\n",mdc(a,b));
+    return 0;
+}
